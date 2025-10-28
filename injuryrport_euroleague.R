db1829ca8799f3f73c264df8e496ef657a8084f1
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-10-28 19:10:41 +0100

    Add files via upload

---
+++ injuryrport_euroleague.R
@@ -0,0 +1,22 @@
+library(dplyr) # for data wrangling
+library(jsonlite) # for getting data 
+
+# get today's date
+today <- Sys.Date() 
+
+# ensure data output folder exists
+if (!dir.exists("2026/euroleague_rotoworld/data")) dir.create("2026/euroleague_rotoworld/data")
+
+# url for getting mvp odds from rotowire
+url <- "https://www.rotowire.com/euro/tables/injury-report.php?team=ALL&pos=ALL"
+
+# get data
+result <- fromJSON(txt=url) 
+
+# add today's date as a column
+df <- result %>% 
+  select(-rDate) %>% 
+  mutate(date = today)
+
+# write dataframe to .csv in a folder called "data/"
+write.csv(df, paste0("2026/euroleague_rotoworld/data/euroleage_injuryreport_", gsub("-", "_", today), ".csv"), row.names = F) 
